e77f7e30bdf66af5ad1e0bb58e11abf2a0392745
Tyler-Williams <github@tylerclarkwilliams.com>
2018-01-10 16:08:46 -0800

    Installed and configured Joi for form validation

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,7 +1,8 @@
+const AuthenticationController = require('./controllers/AuthenticationController')
+const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
+
 module.exports = (app) => {
-  app.post('/register', (req, res) => {
-    res.send({
-      message: `Hello ${req.body.email}! Your user was registerd! Have fun!`
-    })
-  })
+  app.post('/register',
+    AuthenticationControllerPolicy.register,
+    AuthenticationController.register)
 }
